aaaba999bff9f7178a63c0d52f68faa78b5cfb71
ed <allgoodata@outlook.com>
2024-02-08 18:44:39 +0200

    DataInflow component

---
+++ src/app/components/pages/home/dataInflow/index.tsx
@@ -0,0 +1,11 @@
+export default function DataInflow() {
+  return (
+    <div>
+      <input placeholder="date" />
+      <input placeholder="category" />
+      <input placeholder="price" />
+      <input placeholder="note" />
+      <button>Confirm</button>
+    </div>
+  );
+}
--- src/app/components/pages/home/index.tsx
+++ src/app/components/pages/home/index.tsx
@@ -1,11 +1,23 @@
 import LayoutWrapper from '../../layoutWrapper';
+import DataInflow from './dataInflow';
 
 export default function HomePage() {
   return (
     <LayoutWrapper>
       <section className="flex gap-[10px] h-[100%] md:flex-col">
-        <div className="w-[20%] bg-[#C6E0FF] rounded-[10px] p-[10px] md:w-[100%] ">
-          Data Input
+        <div className="flex flex-col w-[20%] rounded-[10px] h-[100%] md:w-[100%] ">
+          <div className="flex">
+            <p className="w-[50%] text-center p-[10px] bg-[#C6E0FF] rounded-t-[10px]">
+              Expense
+            </p>
+            <p className="w-[50%] text-center p-[10px] bg-[white] rounded-e-[10px]">
+              Income
+            </p>
+          </div>
+          <div className="flex flex-col  p-[10px] h-[100%] bg-[#C6E0FF] rounded-e-[10px] rounded-b-[10px]">
+          <p className='pt-[10px] pb-[10px] font-bold'>Expense</p>
+            <DataInflow />
+          </div>
         </div>
         <div className="w-[80%] bg-[#91F5AD] rounded-[10px] p-[10px] md:w-[100%]">
           Budget Setter
