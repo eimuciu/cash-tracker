fe5812c6fdfd1de94b32fd2f96a1995c4a66cd79
ed <allgoodata@outlook.com>
2024-02-08 21:49:22 +0200

    creating datepicker

---
+++ src/app/components/pages/home/dataInflow/datepicker/index.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React from 'react';
+
+const commonClasses = 'w-[100%] mb-[10px] p-[5px] rounded bg-[white]';
+
+export default function DatePicker() {
+  const [dateValue, setDateValue] = React.useState<string>();
+
+  const refEl = React.useRef<HTMLInputElement>(null);
+
+  React.useEffect(() => {
+    setDateValue(new Date().toLocaleDateString());
+  }, []);
+
+  const changeHandler = (e: any) => {
+    setDateValue(new Date(e.target.value).toLocaleDateString());
+  };
+
+  return (
+    <div className="relative">
+      <div
+        onClick={() => {
+          if (refEl.current) {
+            refEl.current.showPicker();
+          }
+        }}
+        className={commonClasses + ' cursor-pointer'}
+      >
+        {dateValue ? dateValue : new Date().toLocaleDateString()}
+      </div>
+      <div className="absolute top-0 w-[0px] h-[0px] invisible">
+        <input ref={refEl} type="date" onChange={changeHandler} />
+      </div>
+    </div>
+  );
+}
--- src/app/components/pages/home/dataInflow/index.tsx
+++ src/app/components/pages/home/dataInflow/index.tsx
@@ -1,15 +1,15 @@
+import DatePicker from "./datepicker";
+
 const commonClasses = 'w-[100%] mb-[10px] p-[5px] rounded';
 
 export default function DataInflow() {
   return (
     <div className="flex flex-col h-[100%]">
-      <input className={commonClasses + ' '} placeholder="date" />
+      {/* <input className={commonClasses + ' '} placeholder="date" /> */}
+      <DatePicker />
       <input className={commonClasses + ' '} placeholder="category" />
       <input className={commonClasses + ' '} placeholder="0.00" type="text" />
-        <textarea
-          className={commonClasses + ' ' + 'grow'}
-          placeholder="note"
-        />
+      <textarea className={commonClasses + ' ' + 'grow'} placeholder="note" />
       <button className="text-center w-[100%] rounded bg-[#F5F5F5] border border-black border-solid py-[5px] mt-[5px]">
         Confirm
       </button>
