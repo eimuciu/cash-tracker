ab2ba3cbc86e8cde5127fe44228fa26de6361a11
ed <allgoodata@outlook.com>
2024-02-12 12:16:02 +0200

    adding icons to the list

--- src/app/components/pages/list/listElement/index.tsx
+++ src/app/components/pages/list/listElement/index.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function ListElement() {
   return (
     <div className="w-[75%] my-[20px] mx-auto flex items-center gap-[10px] md:w-[95%]">
-      <div className="w-[20%]">icon</div>
+      <div className="w-[20%]"><div className="w-[25px] h-[25px] relative"><Image src='/burger.png' alt="icon" fill /></div></div>
       <div className="w-[35%]">
         <p className="font-bold">Food</p>
         <p>02.02.2024</p>
