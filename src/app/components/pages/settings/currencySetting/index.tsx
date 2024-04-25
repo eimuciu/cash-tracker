b39fae32d204c2911b39e453c255abbd86c5b92e
eimuciu <allgoodata@outlook.com>
2024-04-25 11:34:43 +0300

    Creating New Currency feature

--- src/app/components/pages/settings/currencySetting/index.tsx
+++ src/app/components/pages/settings/currencySetting/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { colorPalette } from '@/utils/themeUnits';
 import { MdLibraryAdd } from 'react-icons/md';
 
@@ -15,10 +15,20 @@ export default function CurrencySetting() {
   const [activeCurrency, setActiveCurrency] = useState<string>(
     settingData[0].currency,
   );
+  const [currenciesList, setCurrenciesList] = useState(settingData);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const addNewCurrency = () => {
+    if (inputRef.current && inputRef.current.value) {
+      const inputValue = inputRef.current.value;
+      setCurrenciesList((prev) => [...prev, { currency: inputValue }]);
+      inputRef.current.value = '';
+    }
+  };
+
   return (
     <div className="flex flex-col items-center w-[100%] gap-[10px]">
       <div className="w-[50%] flex flex-col gap-[5px]">
-        {settingData.map((x) => (
+        {currenciesList.map((x) => (
           <div
             key={x.currency}
             className={
@@ -36,13 +46,17 @@ export default function CurrencySetting() {
       </div>
       <div className="w-[100%] flex gap-[5px] items-center">
         <input
+          ref={inputRef}
           className="w-[100%] rounded-[5px] py-[2.5px] px-[10px] outline-none"
           placeholder="Type..."
         />
-        <MdLibraryAdd className="h-[30px] w-[30px] color-[red] cursor-pointer" />
+        <MdLibraryAdd
+          onClick={addNewCurrency}
+          className="h-[30px] w-[30px] color-[red] cursor-pointer"
+        />
       </div>
       <div className="w-[50%] bg-[#F5F5F5] text-center rounded-[5px] py-[2.5px]">
-      <button className='w-[100%] h-[100%]'>Confirm</button>
+        <button className="w-[100%] h-[100%]">Confirm</button>
       </div>
     </div>
   );
