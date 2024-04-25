b3975195cce0827aa704ed3a68bc9db7e599023d
eimuciu <allgoodata@outlook.com>
2024-04-25 16:02:07 +0300

    Icon selector styling

--- src/app/components/pages/settings/iconsSetting/iconSelector/index.tsx
+++ src/app/components/pages/settings/iconsSetting/iconSelector/index.tsx
@@ -1,15 +1,29 @@
 import Image from 'next/image';
 import iconPathList from '../../../../../../../iconPaths.json';
 
-export default function IconSelector() {
+interface Props {
+  showSelector: boolean;
+  onIconSelection: (x: string) => void;
+}
+
+export default function IconSelector({ showSelector, onIconSelection }: Props) {
   return (
-    <div className="flex flex-wrap gap-[10px] justify-center ">
+    <div
+      className={
+        'flex flex-wrap gap-[10px] justify-center' +
+        ' ' +
+        `${!showSelector && 'hidden'}`
+      }
+    >
       {iconPathList.map((x) => (
         <div
           key={x}
+          onClick={() => {
+            onIconSelection(x);
+          }}
         >
           <div className="w-[50px] h-[50px] relative cursor-pointer">
-            <Image src={x} fill alt='icon' />
+            <Image src={x} fill alt="icon" />
           </div>
         </div>
       ))}
--- src/app/components/pages/settings/iconsSetting/index.tsx
+++ src/app/components/pages/settings/iconsSetting/index.tsx
@@ -1,36 +1,58 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import IconSelector from './iconSelector';
+import { colorPalette } from '@/utils/themeUnits';
 
 {
   /* AWAITING DATA */
 }
 const settingData = [
-  { category: 'Food', icon: '/burger.png' },
-  { category: 'Car', icon: '/car.png' },
-  { category: 'Sport', icon: '/sport.png' },
+  { category: 'Food', icon: '/icons/burger.png' },
+  { category: 'Car', icon: '/icons/car.png' },
+  { category: 'Sport', icon: '/icons/sport.png' },
   { category: 'Clothes', icon: '/icons/clothing.png' },
   { category: 'Rent', icon: '/icons/house.png' },
   { category: 'Holiday', icon: '/icons/cellphone.png' },
 ];
 
 export default function IconsSetting() {
+  const [iconsList, setIconsList] = useState(settingData);
+  const [activeIcon, setActiveIcon] = useState<any>(null);
+
+  const handleIconSelection = (path: string) => {
+    setIconsList((prev) =>
+      prev.map((x) =>
+        x.category == activeIcon.category ? { ...x, icon: path } : x,
+      ),
+    );
+    setActiveIcon(null);
+  };
+
   return (
     <div className="flex flex-col items-center w-[100%] gap-[10px]">
       <div className="w-[50%] flex flex-col gap-[5px]">
-        {settingData.map((x) => (
+        {iconsList.map((x) => (
           <div
             key={x.category}
-            className={'text-center rounded-[5px] flex justify-between'}
+            className={
+              'text-center rounded-[5px] flex justify-between px-[10px] py-[2.5px] cursor-pointer' +
+              ' ' +
+              ` bg-[${activeIcon?.category == x.category && colorPalette.gingered}]`
+            }
+            onClick={() => setActiveIcon(x)}
           >
             {x.category}
-            <div className="w-[25px] h-[25px] relative cursor-pointer">
+            <div className="w-[25px] h-[25px] relative">
               <Image src={x.icon} fill alt="icon" />
             </div>
           </div>
         ))}
       </div>
-      <div>
-        <IconSelector />
+      <div className='mt-[10px]'>
+        <IconSelector
+          showSelector={Boolean(true)}
+          onIconSelection={handleIconSelection}
+        />
       </div>
       <div className="w-[50%] bg-[#F5F5F5] text-center rounded-[5px] py-[2.5px]">
         <button className="w-[100%] h-[100%]">Confirm</button>
