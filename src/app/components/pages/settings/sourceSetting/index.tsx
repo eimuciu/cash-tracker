61a50a978b2720eb423240d16c949f6bab7d4d31
eimuciu <allgoodata@outlook.com>
2024-04-26 20:59:11 +0300

    Adding input to source setting

--- src/app/components/pages/settings/sourceSetting/index.tsx
+++ src/app/components/pages/settings/sourceSetting/index.tsx
@@ -1,3 +1,4 @@
+import { useState, useRef } from 'react';
 import { MdLibraryAdd } from 'react-icons/md';
 import { FaTrashAlt } from 'react-icons/fa';
 
@@ -7,25 +8,47 @@ import { FaTrashAlt } from 'react-icons/fa';
 const settingData = ['Salary', 'Investment'];
 
 export default function SourceSetting() {
+  const [sourceList, setSourceList] = useState(settingData);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addNewSource = () => {
+    if (inputRef.current && inputRef.current.value) {
+      const inputValue = inputRef.current.value;
+      setSourceList((prev) => [...prev, inputValue]);
+      inputRef.current.value = '';
+    }
+  };
+
+  const removeSourceFromList = (sourceName: string) => {
+    setSourceList((prev) => prev.filter((x) => x !== sourceName));
+  };
+
   return (
     <div className="flex flex-col items-center w-[100%] gap-[10px]">
       <div className="w-[50%] flex flex-col gap-[5px]">
-        {settingData.map((x) => (
+        {sourceList.map((x) => (
           <div
             key={x}
             className={'text-center rounded-[5px] flex justify-between'}
           >
             {x}
-            <FaTrashAlt className="cursor-pointer" />
+            <FaTrashAlt
+              onClick={() => removeSourceFromList(x)}
+              className="cursor-pointer"
+            />
           </div>
         ))}
       </div>
       <div className="w-[100%] flex gap-[5px] items-center">
         <input
+          ref={inputRef}
           className="w-[100%] rounded-[5px] py-[2.5px] px-[10px] outline-none"
           placeholder="Type..."
         />
-        <MdLibraryAdd className="h-[30px] w-[30px] color-[red] cursor-pointer" />
+        <MdLibraryAdd
+          onClick={addNewSource}
+          className="h-[30px] w-[30px] color-[red] cursor-pointer"
+        />
       </div>
       <div className="w-[50%] bg-[#F5F5F5] text-center rounded-[5px] py-[2.5px]">
         <button className="w-[100%] h-[100%]">Confirm</button>
