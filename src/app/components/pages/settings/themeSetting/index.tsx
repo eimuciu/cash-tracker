5afc84ea9a7ce28db8ca2532db88ba6b49130cee
eimuciu <allgoodata@outlook.com>
2024-04-27 10:45:19 +0300

    Theme picker construction

--- src/app/components/pages/settings/themeSetting/index.tsx
+++ src/app/components/pages/settings/themeSetting/index.tsx
@@ -1,24 +1,9 @@
-'use client';
-
-import React from 'react';
-
-// AWAITING DATA START
-const expenseCategory = ['Exone', 'Extwo', 'Exthree', 'Exfour'];
-const incomeCategory = ['Inone', 'Intwo', 'Inthree', 'Infour'];
-// AWAITING DATA FINISH
-
 export default function ThemeSetting() {
-  const [tab, setTab] = React.useState<string>('expense');
-
-  const handlePickTab = (tabName: string) => {
-    setTab(tabName);
-  };
-
   return (
-    <div className="flex bg-[white]">
+    <div className="flex bg-[white] rounded-[10px]">
       <div className="w-[100%] gap-[10px] pl-[91.125px] pr-[25px] py-[25px] md:pl-[25px] flex flex-col">
-        <section className="flex w-[50%] h-[50px] flex-col items-center bg-[#D8D4F2] rounded-[10px] p-[10px]"></section>
-        <div className="h-[25px] w-[50%] rounded-[10px] bg-[#d1ac00]"></div>
+        <section className="flex w-[50%] h-[50px] flex-col items-center bg-[#D8D4F2] rounded-[10px] p-[10px] md:w-[100%]"></section>
+        <div className="h-[25px] w-[50%] rounded-[10px] bg-[#d1ac00] md:w-[100%]"></div>
         <section className="flex gap-[10px] flex-col">
           <div className="flex flex-col w-[50%] rounded-[10px] md:w-[100%] ">
             <div
@@ -27,7 +12,7 @@ export default function ThemeSetting() {
               }
             ></div>
           </div>
-          <div className="w-[50%] h-[100px] bg-[#91F5AD] rounded-[10px] p-[10px] md:w-[100%]"></div>
+          <div className="w-[50%] h-[100px] bg-[#91F5AD] rounded-[10px] md:w-[100%]"></div>
         </section>
       </div>
     </div>
