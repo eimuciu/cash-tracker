a7fbb03e2a720bf4465adaf8868d7e2ecc1bdd30
eimuciu <allgoodata@outlook.com>
2024-04-28 14:23:49 +0300

    Activating active color picker

--- src/app/components/pages/settings/themeSetting/index.tsx
+++ src/app/components/pages/settings/themeSetting/index.tsx
@@ -1,12 +1,36 @@
+import React, { ChangeEvent } from 'react';
 import { colorPalette } from '@/utils/themeUnits';
 const { greened, gingered, reded, violeted, greyed, blueued } = colorPalette;
 
 export default function ThemeSetting() {
+  const [activeColor, setActiveColor] = React.useState<string>(reded);
+
+  const refEl = React.useRef<HTMLInputElement>(null);
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setActiveColor(e.target.value);
+  };
+
   return (
     <div className="flex bg-[white] rounded-[10px]">
       <div className="w-[100%] gap-[10px]  px-[25px] py-[25px] md:pl-[25px] flex flex-col">
-        <div className={`text-[${reded}] font-bold flex cursor-pointer`}>
+        <div
+          onClick={() => {
+            if (refEl.current) {
+              refEl.current.showPicker();
+            }
+          }}
+          className={`font-bold flex cursor-pointer relative`}
+          style={{color: activeColor}}
+        >
           Active
+          <div className="absolute top-0 w-[0px] h-[0px] invisible">
+            <input
+              ref={refEl}
+              type="color"
+              onChange={changeHandler}
+            />
+          </div>
         </div>
         <div className="flex w-[100%] h-[50px] flex-col items-center bg-[#D8D4F2] rounded-[10px] p-[10px] md:w-[100%] cursor-pointer"></div>
         <div className="h-[25px] w-[100%] rounded-[10px] bg-[#d1ac00] md:w-[100%] cursor-pointer"></div>
