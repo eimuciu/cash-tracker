30d81d8e67bae2b24a5d4d85eaa82361e8f833d8
eimuciu <allgoodata@outlook.com>
2024-05-01 17:57:05 +0300

    Creating data types for db

---
+++ src/utils/types.ts
@@ -0,0 +1,59 @@
+export const settings = {
+  budget: {
+    '2024-01': [],
+    '2024-02': [],
+    '2024-03': [],
+    '2024-04': [],
+    '2024-05': [
+      { budget: 200, category: 'car' },
+      { budget: 200, category: 'food' },
+    ],
+  },
+  colors: {
+    alcohol: 'red',
+    books: 'green',
+    sport: 'blue',
+  },
+  currency: '€',
+  expenseCategories: ['alcohol', 'books', 'sport'],
+  incomeSource: ['job', 'selling', 'interests', 'investment'],
+  theme: {
+    activeColor: '#880D1E',
+    firstColor: '#D8D4F2',
+    secondColor: '#D1AC00',
+    thirdColor: '#C6E0FF',
+    fourthColor: '#91F5AD',
+    greyedColor: '#F5F5F5',
+  },
+  currenciesList: [],
+};
+
+export const expenseData = [
+  {
+    amount: 5,
+    category: 'food',
+    date: '2024-05-01',
+    note: 'traskuciai, energetinis gerimas',
+  },
+  {
+    amount: 10,
+    category: 'car',
+    date: '2024-05-01',
+    note: 'degalai degalineje',
+  },
+];
+
+export const incomeData = [
+  {
+    amount: 1000,
+    soure: 'job',
+    date: '2024-05-01',
+    note: 'alga',
+  },
+  {
+    amount: 500,
+    soure: 'selling',
+    date: '2024-05-01',
+    note: 'pardaviau kompa',
+  },
+];
