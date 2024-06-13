051022e512bd248da740c0f001ae4abbff5a0691
eimuciu <allgoodata@outlook.com>
2024-06-13 18:47:14 +0300

    header refacto

--- src/app/components/layoutWrapper/header/index.tsx
+++ src/app/components/layoutWrapper/header/index.tsx
@@ -2,9 +2,24 @@ import { useThemeContext } from '@/app/store/themeStore';
 import { useDataContext } from '@/app/store/dataStore';
 import { sumData } from '@/utils/sumData';
 
+const monthList = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
 export default function Header() {
   const { themeColorsList }: any = useThemeContext();
-  const { settings, incomeList, expenseList }: any = useDataContext();
+  const { settings, incomeList, expenseList, filter }: any = useDataContext();
 
   return (
     <>
@@ -14,8 +29,19 @@ export default function Header() {
       >
         <div className="font-bold mb-[5px]">
           <p className="text-[20px]">
-            {/* AWAITING DATA */}
-            AWAITING
+            {filter.case === 'THIS_MONTH' && monthList[new Date().getMonth()]}
+            {filter.case === 'LAST_MONTH' &&
+              monthList[new Date().getMonth() - 1]}
+            {filter.case === 'THIS_YEAR' && new Date().getFullYear()}
+            {filter.case === 'CUSTOM_DATE' &&
+            filter.options.startDate &&
+            filter.options.finishDate
+              ? `${new Date(
+                  filter.options.startDate,
+                ).toLocaleDateString()} - ${new Date(
+                  filter.options.finishDate,
+                ).toLocaleDateString()}`
+              : null}
           </p>
         </div>
         <div className="flex w-[50%] justify-around sm:w-[75%] xs:w-[100%]">
